852746322adcdbc07ce2b99a4da22f14aa5c61ca
Timothy Walker <tpw32@scarletmail.rutgers.edu>
2022-03-11 20:05:34 -0500

    Layout Changes + Animations

--- home.js
+++ home.js
@@ -1,7 +1,7 @@
 
 
 let headerText = document.getElementById("headerText");
-const headerText1 = "</ Timothy Walker >"
+const headerText1 = "</ Tim Walker >"
 
 let index = 0;
 function writeHeader() {
@@ -32,3 +32,28 @@ window.onscroll = function(){
 			document.getElementById("projectsHeader").style.opacity=0;
 		} 
 };
+
+var slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+	showSlides(slideIndex += n);
+  }
+function currentSlide(n) {
+	showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName("project");
+	var dots = document.getElementsByClassName("dot");
+	if (n > slides.length) {slideIndex = 1}
+	if (n < 1) {slideIndex = slides.length}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
+	slides[slideIndex-1].style.display = "flex";
+	dots[slideIndex-1].className += " active";
+  }
